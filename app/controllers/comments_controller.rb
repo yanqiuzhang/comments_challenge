c81f100dec751f198a37ac9a1c8426108383c130
olivia <olivia@evolve.se>
2019-02-24 21:14:42 +0100

    add comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,11 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    # redirect_to new_article_path(@article)
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:email, :comment)
     end
 end
